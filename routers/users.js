7cd98b82ad74743540d28fce26f60811fefd60d3
Daniel Kang <d.kang106@gmail.com>
2018-12-15 22:06:41 -0800

    Added routers and views

---
+++ routers/users.js
@@ -0,0 +1,63 @@
+const
+    express = require('express'),
+    usersRouter = new express.Router(),
+    passport = require('passport')
+
+// render login view
+usersRouter.get('/login', (req, res) => {
+    res.render('login')
+});
+
+// login route
+usersRouter.post('/login', passport.authenticate('local-login', {
+    successRedirect: 'users/profile',
+    failureRedirect: 'users/login'
+}));
+
+// render signup view
+usersRouter.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// signup route
+usersRouter.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: 'users/profile',
+    failureRedirect: '/users/signup'
+}));
+
+// require login to view profile
+usersRouter.get('/profile', isLoggedIn, ({ user }, res) => {
+    res.render('profile', { user });
+});
+
+// route to update users password
+usersRouter.patch('/profile', isLoggedIn, (req, res) => {
+    if (!req.body.password) delete req.body.password;
+    // merge req.body into req.user object
+    Object.assign(req.user, req.body);
+    req.user.save((err, updatedUser) => {
+        if (err) console.log(err);
+        res.redirect('/users/profile');
+    });
+});
+
+
+// Edit Route using method override
+usersRouter.get('/profile/edit', isLoggedIn, (req, res) => {
+    res.render('editProfile');
+});
+
+// Logout Route
+usersRouter.get('/logout', (req, res) => {
+    // destroys current session and sends back to landing page
+    req.logout(); 
+    res.redirect('/');
+});
+
+// check if the user is logged in
+function isLoggedIn(req, res, next) {
+    // isAuthenticted checks the cookie
+    if (req.isAuthenticated()) return next();
+    // if not authenticated, redirect to login view
+    res.redirect('/users/login');
+}
